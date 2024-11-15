3b5eb14db02314f4e77ae3fcb10bf19677b8184b
Kevin <4932439-kev-m@users.noreply.gitlab.com>
2024-11-15 15:51:48 +0100

    Bumping version.

--- fitout/__init__.py
+++ fitout/__init__.py
@@ -1,6 +1,6 @@
 """A Python library to extract FitBit Google Takeout data."""
 # Semantic Versioning according to https://semver.org/spec/v2.0.0.html
-__version__ = "v0.0.7"  # Adding basic sleep summary data
+__version__ = "v0.0.8"  # fix: Documenting basic sleep API.
 
 
 from .helpers import *
