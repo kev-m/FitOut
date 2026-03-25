ab7a2acbd320e836d1576bb1f39be61bc3edabf2
Kevin <4932439-kev-m@users.noreply.gitlab.com>
2026-03-25 11:21:28 +0100

    Bumping version: "v0.3.1"  # fix: Re-applying heart-rate caching fix.

--- fitout/__init__.py
+++ fitout/__init__.py
@@ -1,6 +1,6 @@
 """A Python library to extract FitBit Google Takeout data."""
 # Semantic Versioning according to https://semver.org/spec/v2.0.0.html
-__version__ = "v0.3.0"  # feat: Added exercise and weight feature importers
+__version__ = "v0.3.1"  # fix: Re-applying heart-rate caching fix.
 
 
 from .helpers import *
